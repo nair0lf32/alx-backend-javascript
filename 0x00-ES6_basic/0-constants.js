0c732216663467d45a7937eb1fd0d1a2ce6e5a73
nair0lf32 <florianedem@gmail.com>
2022-12-20 13:56:08 +0100

    review tasks

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,9 @@
-export function taskBlock(trueOrFalse) {
+/* eslint-disable linebreak-style */
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
   }
 
