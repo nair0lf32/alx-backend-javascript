275088b233881571b93f13b859cf0843b7d17800
nair0lf32 <florianedem@gmail.com>
2022-12-21 13:37:26 +0100

    fix task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((_resolve, _reject) => {
-    if (success) _resolve({ status: 200, body: 'success' });
+    if (success) _resolve({ status: 200, body: 'Success' });
     else _reject(Error('The fake API is not working currently'));
   });
 }
