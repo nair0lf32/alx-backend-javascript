ed798b122aa1e5bdf0a2023f225ae062239e9f4b
nair0lf32 <florianedem@gmail.com>
2022-12-21 13:26:40 +0100

    fix task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,4 @@
 export default function getFullResponseFromAPI(success) {
-  // eslint-disable-next-line no-unused-vars
   return new Promise((_resolve, _reject) => {
     if (success) _resolve({ status: 200, body: 'success' });
     else _reject(Error('The fake API is not working currently'));
