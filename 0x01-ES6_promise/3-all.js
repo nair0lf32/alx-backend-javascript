1af33101ffe4f742982933838107e8f481bc13d0
nair0lf32 <florianedem@gmail.com>
2022-12-21 14:00:14 +0100

    fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,10 @@
 import { uploadPhoto, createUser } from 'utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((result) => { console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`); })
+  return Promise.all([uploadPhoto(), createUser()]).then((result) => {
+    console.log(`${result[0].body} 
+    ${result[1].firstName} 
+    ${result[1].lastName}`);
+  })
     .catch(() => { console.log('Signup system offline'); });
 }
