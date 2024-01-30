1ced0786436ea4cdf5f5a2e6bfc382e38fce7b27
EDEMESSI Florian <florianedem@gmail.com>
2024-01-30 23:11:10 +0100

    update stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   },
   plugins: ['jest'],
   rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
     'no-console': 'off',
     'no-shadow': 'off',
     'no-restricted-syntax': [
