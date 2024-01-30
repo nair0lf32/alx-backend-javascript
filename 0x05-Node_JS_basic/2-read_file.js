b2c615edc73ec5da15c6cc133cd69b180f5a2043
EDEMESSI Florian <florianedem@gmail.com>
2024-01-31 00:37:08 +0100

    do tasks

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,40 +1,56 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-param-reassign */
 /* eslint-disable prefer-destructuring */
+/* eslint-disable guard-for-in */
+
 const fs = require('fs');
 
 function countStudents(path) {
-  path = process.argv[2];
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split(/\r?\n/);
-    let count = lines.length;
-    if (lines[lines.length - 1] === '') {
-      count -= 1;
+
+    // Remove empty lines
+    const nonEmptyLines = lines.filter((line) => line.trim() !== '');
+
+    if (nonEmptyLines.length === 0) {
+      throw new Error('No valid students found in the database');
     }
-    console.log(`Number of students: ${count}`);
+
+    console.log(`Number of students: ${nonEmptyLines.length}`);
+
     const fields = {};
     const students = {};
-    for (const line of lines) {
-      if (line !== lines[0]) {
-        const student = line.split(',');
-        if (!fields[student[3]]) {
-          fields[student[3]] = [];
-        }
-        fields[student[3]].push(student[0]);
-        students[student[0]] = {
-          id: student[0],
-          firstName: student[1],
-          lastName: student[2],
-          field: student[3],
-        };
+
+    for (const line of nonEmptyLines) {
+      const student = line.split(',');
+
+      if (student.length !== 4) {
+        throw new Error('Invalid data format in the database');
       }
+
+      if (!fields[student[3]]) {
+        fields[student[3]] = [];
+      }
+
+      fields[student[3]].push(student[1]);
+
+      students[student[0]] = {
+        id: student[0],
+        firstName: student[1],
+        lastName: student[2],
+        field: student[3],
+      };
     }
-    console.log(`Number of students in CS: ${fields.CS.length}. List: ${fields.CS.join(', ')}`);
-    console.log(`Number of students in SWE: ${fields.SWE.length}. List: ${fields.SWE.join(', ')}`);
+
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+
     return students;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
