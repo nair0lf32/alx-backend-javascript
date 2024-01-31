2a6f67480a37926537be057d92caeae4bda82f3f
EDEMESSI Florian <florianedem@gmail.com>
2024-01-31 02:17:44 +0100

    do tasks

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  switch (req.url) {
+    case '/':
+      res.end('Hello Holberton School!');
+      break;
+    default:
+      res.end();
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+export default app;
